9acd675d5fd68986cdd57e2015d49b53bbe77f22
LordMous <90604774+LordMous@users.noreply.github.com>
2022-09-13 09:09:41 +0200

    Add files via upload

---
+++ 106.js
@@ -0,0 +1,9 @@
+let szam=500;
+if(szam%2===0){
+    console.log("Paros")
+}else{
+    console.log("Paratlan")
+}
+
+// ==ertek
+/// ===ertek es tipus is 
---
+++ Bajnok.js
@@ -0,0 +1,56 @@
+let nev = "Zolika";
+alert(nev);
+let toritanar, magyartanar, matektanar;
+toritanar = "Kubik József";
+magyartanar = "Dr. Egriné Opauszky Mária Eszter 3.";
+matektanar = "szekelynezsu";
+toritanar = "Sós Tanar";
+console.log(toritanar);
+//const ofo = "Kriszta";
+//ofo = "Anyaneni";
+//console.log(ofo);
+let str = "Szöveg";
+let int = 11;
+let log = false;
+console.log(typeof log);
+
+let szamm = 23;
+let szo = "string";
+let felnott = false;
+
+console.log(typeof felnott);
+console.log(typeof szo);
+console.log(typeof szamm);
+
+let termek1;
+console.log(typeof termek1);
+
+let ar = 3100;
+const PI = 3.14;
+
+console.log(typeof ar);
+console.log(typeof PI);
+
+ar++;
+console.log(ar);
+ar+=10; //ar=ar+10
+ar-=5; 
+ar*=2;
+//ar/=2;
+//ar%=5;
+
+if(ar>1000){
+    console.log("nagyobb mint 1000")
+}else{
+    console.log("kisebb mint 1000")
+}
+
+if(ar>7000){
+    console.log("Nagyobb mint 7000")
+}else if(ar>5000){
+    console.log("5000-től nagyobb")
+}else{
+    console.log("Kisebb")
+}
+
+//feladat: uj valtozo tetszoleges ertekkel , paros vagy paratlan
